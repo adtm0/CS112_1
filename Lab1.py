fdd3515ec78fccd1872e8f0c7e1acb1898ee7d20
adtm0 <145269995+adtm0@users.noreply.github.com>
2023-09-20 05:33:17 -0700

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -1,8 +1,8 @@
 print("""
-Complete Name: 
-Course: 
-Section: 
-Age: 
-Birthday: 
-Complete Home Address: 
+Complete Name: Aubrey Malanog
+Course: BS in Computer Science
+Section: CS1E
+Age: 21
+Birthday: Feb. 14, 2002
+Complete Home Address: Carmen, Cagayan de Oro City
 """)
